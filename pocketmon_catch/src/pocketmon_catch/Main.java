23bb97593f9a5cd228cb83914b2c7836ce132456
SMHRD <SMHRD@121.147.52.191>
2024-01-26 17:30:06 +0900

    BGM 추가

--- pocketmon_catch/src/pocketmon_catch/Main.java
+++ pocketmon_catch/src/pocketmon_catch/Main.java
@@ -1,11 +1,16 @@
 package pocketmon_catch;
 
+import javazoom.jl.player.MP3Player;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		MP3Player mp3 = new MP3Player();
+		BgmDAO bgm = new BgmDAO();
+
 		System.out.println("즐거운 포켓몬월드 캐치!");
-				
+
 	}
 
 }
