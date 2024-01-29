3c0eb365843d8383da8de79715f1f95089d6b661
SMHRD <SMHRD@222.102.104.251>
2024-01-29 09:53:46 +0900

    닉네임 수정 / 아이디 수정 메인 추가

--- pocketmon_catch/src/pocketmon_catch/Main.java
+++ pocketmon_catch/src/pocketmon_catch/Main.java
@@ -18,10 +18,10 @@ public class Main {
 		PlayDAO pldao = new PlayDAO();
 		BgmDAO bgm = new BgmDAO();
 		
-		
+		System.out.println("즐거운 포켓몬월드 캐치!");
+		System.out.println("포켓몬 월드에 오신걸 환영합니다!");
 		while(true) {
 			bgm.ostPlay();
-			System.out.println("즐거운 포켓몬월드 캐치!");
 			System.out.print("[1]로그인 [2]회원가입 [3]랭킹보기 [4]닉네임수정 [5]회원탈퇴 [6]게임종료 ");
 			int choice = sc.nextInt();
 			if(choice==1) {
@@ -45,7 +45,29 @@ public class Main {
 				
 				
 			}else if(choice==2) {
+			      System.out.print("가입할 아이디 : ");
+			      String joinId = sc.next();
+			      
+			      int result = udao.Idcheck(joinId);
+			      
+			      if(result == 0) {
+			         System.out.println("사용가능한 ID 입니다.");
+			      }else {
+			         System.out.println("중복된 ID 입니다.");
+			         continue;
+			      }
+			      
+			      System.out.print("가입할 비밀번호 입력 : ");
+			      String joinPw = sc.next();
+			      System.out.print("가입할 닉네임 입력 : ");
+			      String joinNick = sc.next();
 
+			      UserDTO dto = new UserDTO();
+			      dto.setID(joinId);
+			      dto.setPW(joinPw);
+			      dto.setNICK(joinNick);
+
+			      int cnt = udao.join(dto);
 				
 				
 				
@@ -60,6 +82,16 @@ public class Main {
 				}
 				
 			}else if(choice==4) {
+				System.out.print("수정할 닉네임 입력 : ");
+				String changeNick = sc.next();
+					
+				int result = udao.Idcheck(changeNick); 
+				
+				if(result == 0) {
+					System.out.println("사용가능한 닉네임입니다.");
+				}else {
+					continue;
+				}
 				
 			}else if(choice==5) {
 				
